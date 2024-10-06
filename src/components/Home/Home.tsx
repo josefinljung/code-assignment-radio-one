1f845bcead436724fe1dddf7984ec0c912f4882f
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 20:51:25 +0200

    fix(Home): use Heading component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,17 @@
+import Heading from '../Heading/Heading';
+
 function Home() {
   return (
-    <div className="flex space-y-4 h-screen justify-center items-center">
-      <a
-        href="/channel"
-        className="border-2 text-white text-xl border-red-600 w-max p-6 h-fit"
-      >
-        Get started
-      </a>
+    <div className="flex h-screen justify-center items-center">
+      <div className="grid space-y-4">
+        <Heading heading="Radio app" />
+        <a
+          href="/channel"
+          className="border-2 text-common-white text-xl border-red-600 w-max p-6 h-fit"
+        >
+          Get started
+        </a>
+      </div>
     </div>
   );
 }
