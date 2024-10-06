96ac68921ad10c1033af912bd66e095ca75f878c
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 20:52:34 +0200

    fix(Header): adjust spacing and text color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,13 @@ interface ChannelProps {
 }
 function Header({ data }: ChannelProps) {
   return (
-    <div className="text-white grid gap-4 justify-items-center md:flex justify-center max-md:text-center py-8 md:px-14 px-8 max-w-screen-lg mx-auto">
+    <div className="grid gap-8 justify-items-center md:flex justify-center max-md:text-center py-8 md:px-14 px-8 max-w-screen-lg mx-auto">
       <img
         className="max-md:w-[calc(100vw/4)] min-w-[50px] max-w-[100px]"
         src={data.channel.image}
         alt="Channel logo"
       />
-      <p className="text-white">{data.channel.tagline}</p>
+      <p className="text-light-blue">{data.channel.tagline}</p>
     </div>
   );
 }
