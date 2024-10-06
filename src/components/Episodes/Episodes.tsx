734c4de366d2ac779e327a39d0647e2b6d406f8a
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 19:04:33 +0200

    feat(Episodes): add filter, error handling and loading state

--- src/components/Episodes/Episodes.tsx
+++ src/components/Episodes/Episodes.tsx
@@ -6,29 +6,123 @@ import Episode from './partials/Episode';
 function Episodes() {
   const { programId } = useParams<{ programId: string }>();
   const [episodes, setEpisodes] = useState<EpisodeType[]>([]);
+  const [filteredEpisodes, setFilteredEpisodes] = useState<EpisodeType[]>([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
-  // todo: add filter
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
   useEffect(() => {
-    fetch(
-      `https://api.sr.se/api/v2/episodes/index?format=json&programid=${programId}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
+    setIsLoading(true);
+    setError('');
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.sr.se/api/v2/episodes/index?format=json&programid=${programId}`
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
         setEpisodes(data.episodes);
-      });
+        setFilteredEpisodes(data.episodes);
+
+        // add loading state for 800ms
+        await delay(800);
+        setIsLoading(false);
+      } catch (error) {
+        setError('Results could not be found');
+        setIsLoading(false);
+      }
+    };
+    fetchData();
   }, [programId]);
 
+  // convert date string to timestamp to match episode.publishdateutc format
+  const extractTimestamp = (dateString: string) => {
+    return parseInt(dateString.replace('/Date(', '').replace(')/', ''), 10);
+  };
+
+  const onInputValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchValue(value);
+    filterEpisodes(value);
+  };
+
+  const onInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      filterEpisodes(searchValue);
+    }
+  };
+
+  const filterEpisodes = (value: string) => {
+    if (!value) {
+      setFilteredEpisodes(episodes);
+      return;
+    }
+
+    const isDate = /^\d{4}(-\d{2})?(-\d{2})?$/.test(value);
+    const filtered = isDate ? filterByDate(value) : filterByTitle(value);
+    setFilteredEpisodes(filtered);
+  };
+
+  const filterByDate = (value: string) => {
+    const inputDate = new Date(value).getTime();
+    return episodes
+      .filter(
+        (episode) => extractTimestamp(episode.publishdateutc) >= inputDate
+      )
+      .sort(
+        (a, b) =>
+          extractTimestamp(a.publishdateutc) -
+          extractTimestamp(b.publishdateutc)
+      );
+  };
+
+  const filterByTitle = (value: string) => {
+    return episodes
+      .filter((episode) =>
+        episode.title.toLowerCase().includes(value.toLowerCase())
+      )
+      .sort((a, b) => a.title.localeCompare(b.title));
+  };
+
   return (
-    <div>
+    <div className="grid gap-y-4">
       <a href="/channel" className="text-white">
         Back
       </a>
-      <p className="text-pink-300 text-xl">Episodes</p>
-      {episodes.map((episode) => (
-        <div>
-          <Episode {...episode} />
-        </div>
-      ))}
+
+      <p className="text-pink-600">filter</p>
+      <form>
+        <input
+          type="text"
+          value={searchValue}
+          onChange={onInputValueChange}
+          onKeyDown={onInputKeyDown}
+        />
+      </form>
+      <div>
+        <p className="text-pink-300 text-xl my-2">Episodes</p>
+        {isLoading ? (
+          <p className="text-white">Loading...</p>
+        ) : error ? (
+          <p className="text-white">{error}</p>
+        ) : (
+          <>
+            {filteredEpisodes.map((episode) => {
+              return (
+                <div key={episode.id}>
+                  <Episode {...episode} />
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
     </div>
   );
 }
