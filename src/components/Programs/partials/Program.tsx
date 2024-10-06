a2cbb550689e087d00889a0f0440fafbce113b70
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 20:53:55 +0200

    feat(Program): add anchor element, update ui

--- src/components/Programs/partials/Program.tsx
+++ src/components/Programs/partials/Program.tsx
@@ -2,11 +2,21 @@ import { Program as ProgramType } from '../../../../types/global';
 
 function Program({ ...program }: ProgramType) {
   return (
-    <div>
-      <p>logo of program</p>
-      <p className="text-white">{program.name}</p>
-      <p>program description</p>
-    </div>
+    <a
+      className="grid grid-cols-[80px,1fr] gap-4 items-start border-b border-light-blue/20 py-2"
+      href={`/channel/episodes/${program.id}`}
+    >
+      <img
+        src={program.programimage}
+        className="w-[80px] h-fit"
+        role="presentation"
+        alt="Program logo"
+      />
+      <div>
+        <h2 className="text-common-white text-lg">{program.name}</h2>
+        <p className="text-light-blue text-xs">{program.description}</p>
+      </div>
+    </a>
   );
 }
 
