83941ae4b1bf7c0f091868742c3f7329bd087a78
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 22:12:35 +0200

    fix(Program): adjust styling

--- src/components/Programs/partials/Program.tsx
+++ src/components/Programs/partials/Program.tsx
@@ -3,12 +3,12 @@ import { Program as ProgramType } from '../../../../types/global';
 function Program({ ...program }: ProgramType) {
   return (
     <a
-      className="grid grid-cols-[80px,1fr] gap-4 items-start border-b border-light-blue/20 py-2"
+      className="grid grid-cols-[80px,1fr] gap-4 items-start"
       href={`/channel/episodes/${program.id}`}
     >
       <img
         src={program.programimage}
-        className="w-[80px] h-fit"
+        className="h-[80px] w-fit"
         role="presentation"
         alt="Program logo"
       />
