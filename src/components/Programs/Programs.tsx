4cdc40e8026a9118abb406aade6017da49f9ae02
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 19:33:04 +0200

    feat(Programs): add error handling, loading state

--- src/components/Programs/Programs.tsx
+++ src/components/Programs/Programs.tsx
@@ -7,32 +7,52 @@ interface ProgramsType {
 }
 
 function Programs({ channelId }: ProgramsType) {
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [programs, setPrograms] = useState<ProgramsResponse>();
+
   useEffect(() => {
-    fetch(
-      `https://api.sr.se/api/v2/programs/index?format=json&filter=program.haspod&filtervalue=true&channelid=${channelId}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
+    setIsLoading(true);
+    setError('');
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.sr.se/api/v2/programs/index?format=json&filter=program.haspod&filtervalue=true&channelid=${channelId}`
+        );
+        if (!response.ok) {
+          throw new Error('No programs could be found.');
+        }
+        const data = await response.json();
         setPrograms(data);
-      });
+
+        setIsLoading(false);
+      } catch (error) {
+        setError('Results could not be found');
+        setIsLoading(false);
+      }
+    };
+    fetchData();
   }, [channelId]);
 
   return (
     <div>
       <h1 className="text-pink-300 text-lg">PROGRAMS</h1>
 
-      <div className="grid gap-2">
-        {programs?.programs.map((program) => {
-          return (
-            <a href={`/channel/episodes/${program.id}`}>
-              <Program {...program} />
-            </a>
-          );
-        })}
-      </div>
+      {isLoading ? (
+        <p className="text-white">Loading...</p>
+      ) : error ? (
+        <p className="text-white">{error}</p>
+      ) : (
+        <div className="grid gap-2">
+          {programs?.programs.map((program) => {
+            return (
+              <a href={`/channel/episodes/${program.id}`} key={program.id}>
+                <Program {...program} />
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
