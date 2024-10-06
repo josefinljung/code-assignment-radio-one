e14cc8211f52c3d15ddcc49dc893a218678439e7
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 22:25:24 +0200

    chore(config): sort colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-pink': 'rgb(206, 8, 100)',
         'bubblegum-pink': 'rgb(240, 162, 209)',
-        'light-blue': 'rgb(129, 146, 169)',
         'common-white': 'rgb(255, 255, 255)',
         'dark-gray-blue': 'rgb(40, 51, 65)',
+        'dark-pink': 'rgb(206, 8, 100)',
+        'light-blue': 'rgb(129, 146, 169)',
       },
 
     },
