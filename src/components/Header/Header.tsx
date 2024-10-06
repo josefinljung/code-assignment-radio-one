199b60819b53b3fc3f50f5b0a7567f7e47914a6f
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-06 22:10:51 +0200

    fix(Header): adjust size of channel logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,14 @@ import { ChannelResponse } from '../../../types/global';
 interface ChannelProps {
   data: ChannelResponse;
 }
+
 function Header({ data }: ChannelProps) {
   return (
     <div className="grid gap-8 justify-items-center md:flex justify-center max-md:text-center py-8 md:px-14 px-8 max-w-screen-lg mx-auto">
       <img
-        className="max-md:w-[calc(100vw/4)] min-w-[50px] max-w-[100px]"
+        className="max-md:h-[calc(100vw/4)] min-h-[50px] max-h-[100px] h-[50px] w-fit"
         src={data.channel.image}
-        alt="Channel logo"
+        alt={`${data.channel.name} logo`}
       />
       <p className="text-light-blue">{data.channel.tagline}</p>
     </div>
