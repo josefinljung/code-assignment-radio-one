58038f4aadb04651561cb5c7125018c6757f635e
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-08 00:02:05 +0200

    feat(NotFound): use Link component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Heading from './../Heading/Heading';
 
 function NotFound() {
@@ -5,9 +6,9 @@ function NotFound() {
     <div className="grid items-center align-center h-screen">
       <div className="text-center">
         <Heading heading="404 - Page not found" />
-        <a className="text-common-white hover:underline" href="/">
+        <Link className="text-common-white hover:underline" to="/">
           Go back to home
-        </a>
+        </Link>
       </div>
     </div>
   );
