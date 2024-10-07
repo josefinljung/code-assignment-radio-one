0b0beab8473ac222a09d6ab45cd7b32d503971d7
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-07 23:20:40 +0200

    fix(Heading): adjuts size prop

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,15 +2,14 @@ import classNames from 'classnames';
 
 interface HeadingProps {
   heading: string;
-  size?: 'sm' | 'md' | 'lg';
+  size?: 'sm' | 'lg';
 }
 
 function Heading({ heading, size }: HeadingProps) {
   return (
     <h1
       className={classNames('text-light-pink text- font-semibold', {
-        'text-lg': size === 'sm',
-        'text-2xl': size === 'md',
+        'text-3xl': size === 'sm',
         'lg:text-5xl text-6xl': size === 'lg',
       })}
     >
