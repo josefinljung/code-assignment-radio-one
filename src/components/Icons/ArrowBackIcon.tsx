95e376227ecaf87bdf403061c749ec7619c81ff6
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-07 23:38:12 +0200

    fix(ArrowbackIcon): adjust props names

--- src/components/Icons/ArrowBackIcon.tsx
+++ src/components/Icons/ArrowBackIcon.tsx
@@ -3,13 +3,13 @@ function ArrowBackIcon() {
     <svg
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="size-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
       />
     </svg>
