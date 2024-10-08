d4732894259e097cd4c332da4d2a359d37067339
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-08 16:36:30 +0200

    fix(css): move color variables to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light-pink': 'rgb(240, 162, 209)',
-        'common-white': 'rgb(255, 255, 255)',
-        'dark-gray-blue': 'rgb(40, 51, 65)',
-        'dark-pink': 'rgb(206, 8, 100)',
-        'dark-purple': 'rgb(75, 10, 130)',
-        'light-blue': 'rgb(129, 146, 169)',
+        'common-white': 'var(--common-white)',
+        'dark-blue': 'var(--dark-blue)',
+        'dark-gray-blue': 'var(--dark-gray-blue)',
+        'dark-pink': 'var(--dark-pink)',
+        'dark-purple': 'var(--dark-purple)',
+        'light-blue': 'var(--light-blue)',
+        'light-pink': 'var(--light-pink)',
       },
 
     },
