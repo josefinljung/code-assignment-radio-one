6e456716c4a8851d923e5df3bdbc7146a8608b70
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-09 19:38:09 +0200

    fix(Header): use correct element

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,14 +6,14 @@ interface HeaderProps {
 
 function Header({ channel }: HeaderProps) {
   return (
-    <div className="grid gap-8 justify-items-center md:flex justify-center items-center max-md:text-center py-8 md:px-14 px-8 max-w-screen-lg mx-auto">
+    <header className="grid gap-8 justify-items-center md:flex justify-center items-center max-md:text-center py-8 md:px-14 px-8 max-w-screen-xl mx-auto">
       <img
         className="max-md:h-[calc(100vw/4)] min-h-[50px] max-h-[100px] h-[100px] w-fit"
         src={channel.image}
         alt=""
       />
       <p className="text-light-blue">{channel.tagline}</p>
-    </div>
+    </header>
   );
 }
 
