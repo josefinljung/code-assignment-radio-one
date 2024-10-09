d3d1d215e0d26016c6d40706b68f694959094634
Josefin Ljung <josefin.ljung@noaignite.com>
2024-10-09 19:40:11 +0200

    fix(Home): update breakpoints

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,7 +4,7 @@ import { Heading, PlayIcon } from '../../components';
 function Home() {
   return (
     <div className="flex h-screen justify-center items-center px-8">
-      <div className="grid gap-8 lg:gap-6 justify-items-center">
+      <div className="grid gap-8 md:gap-6 justify-items-center">
         <div className="flex items-center gap-4 flex-wrap justify-center">
           <div className="p-4 bg-dark-pink rounded-full w-fit">
             <PlayIcon />
@@ -13,7 +13,7 @@ function Home() {
         </div>
         <Link
           to="/channel"
-          className="ease-in-out duration-100 hover:scale-105 transform-scale shadow-[0_0px_700px_-5px_rgba(130,0,255)] bg-gradient-to-r from-dark-pink to-dark-purple rounded-xl text-common-white text-xl  lg:text-xs font-semibold w-max py-4 lg:px-6 px-7 h-fit"
+          className="ease-in-out duration-100 hover:scale-105 transform-scale shadow-[0_0px_700px_-5px_rgba(130,0,255)] bg-gradient-to-r from-dark-pink to-dark-purple rounded-xl text-common-white text-xl md:text-xs font-semibold w-max py-4 md:px-6 px-7 h-fit"
         >
           Get started
         </Link>
